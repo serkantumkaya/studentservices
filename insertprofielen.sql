b4e0f27e9a8b4da668f42e1b45e6d8f1375b08d8
phwmderuijter <58484596+phwmderuijter@users.noreply.github.com>
2019-12-14 22:00:02 +0100

    SQLs om de datbase te vullen
    SQLs om de dabatase te vullen

---
+++ insertprofielen.sql
@@ -0,0 +1,14 @@
+INSERT INTO profiel(Gebruikersnaam, Email, Wachtwoord, School, Opleiding, Startdatumopleiding, Privelege) 
+VALUES ("ruudderooi@gmail.com","ruudderooi@gmail.com",'Welkom123!',1,2,'20193108',0),
+("rh@gmail.com","rh@gmail.com",'Welkom123!',1,2,'20193108',0),
+("MariskaRaamsdonk@gmail.com","MariskaRaamsdonk@gmail.com",'Welkom123!',1,2,'20193108',0),
+("JolandavanHooijdonk@gmail.com","JolandavanHooijdonk@gmail.com",'Welkom123!',1,2,'20193108',0),
+("JoostvanIperen@gmail.com","JoostvanIperen@gmail.com",'Welkom123!',1,2,'20193108',0),
+("psmets@gmail.com","psmets@gmail.com",'Welkom123!',1,2,'20193108',0),
+("O.Jylha@gmail.com","O.Jylha@gmail.com",'Welkom123!',1,2,'20193108',0),
+("NicodeJong@gmail.com","NicoDeJong@gmail.com",'Welkom123!',1,2,'20193108',0),
+("dmuller@gmail.com","dmuller@gmail.com",'Welkom123!',1,2,'20193108',0),
+("MikdeBeer@gmail.com","MikdeBeer@gmail.com",'Welkom123!',1,2,'20193108',0),
+("MiguelVenancio@gmail.com","MiguelVenancio@gmail.com",'Welkom123!',1,2,'20193108',0),
+("LindaVanHeijst@gmail.com","LindaVanHeijst@gmail.com",'Welkom123!',1,2,'20193108',0),
+("Cooijman@gmail.com","Cooijman@gmail.com",'Welkom123!',1,2,'20193108',0)
