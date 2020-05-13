ecef5f6b4da4c94e3a523df8713c5dddf93824e2
Hellsystem28 <j-d-r@live.nl>
2020-05-13 17:28:04 +0200

    added SALT to the login

--- inlogPag.php
+++ inlogPag.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-    <link rel="shortcut icon" type="image/x-icon" href="images/studentservices.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="images/studentservices.ico"/>
     <meta charset="utf-8">
     <title>Student Services</title>
     <meta name="Inloggen" content="index">
@@ -20,23 +20,23 @@
 <body>
 
 <?php
-require_once ($_SERVER['DOCUMENT_ROOT']."/StudentServices/Includes/DB.php");
-require_once ($_SERVER['DOCUMENT_ROOT']."/StudentServices/Controller/GebruikerController.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/StudentServices/Includes/DB.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/StudentServices/Controller/GebruikerController.php");
 session_start();
 $wronglogin = "";
 
 
-if (isset( $_POST['username']) && $_POST['password']){
-    $username   = $_POST['username'];
-    $password   = $_POST['password'];
-    $DB         = new ConnectDB();
-    $password   = hash('sha256',$password);//$DB->MakeSafe($password);
+if (isset($_POST['username']) && $_POST['password']){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $DB       = new ConnectDB();
+    //zet de salt bij het wachtwoord, zo wordt deze veiliger.
+    $pwsafe = $DB->MakeSafe($password);
 
-    $GC         = new GebruikerController();
-    $Gebruiker  = $GC->Validate($username, $password);
+    $GC        = new GebruikerController();
+    $Gebruiker = $GC->Validate($username, $pwsafe);
 
     if ($Gebruiker->getGebruikerID() != -1){
-
         echo "Je wachtwoord was goed echter werkt het doorverwijzen nog niet!";
         $_SESSION["GebruikerID"] = $Gebruiker->getGebruikerID();
         header("Location: index.php");
@@ -52,12 +52,12 @@ if (isset( $_POST['username']) && $_POST['password']){
     <!--styling is tijdelijk-->
     <div class="container">
         <div style="width:100%">
-            <label for='username'  style="width:150px">Gebruikersnaam:</label>
-            <input type='text' name='username'  style="width:150px"/>
+            <label for='username' style="width:150px">Gebruikersnaam:</label>
+            <input type='text' name='username' style="width:150px"/>
         </div>
         <div style="width:100%;padding-top: 5px">
-            <label for='password'  style="width:150px">Wachtwoord:</label>
-            <input type='password' style="width:150px" name='password' />
+            <label for='password' style="width:150px">Wachtwoord:</label>
+            <input type='password' style="width:150px" name='password'/>
         </div>
         <?php
         echo $wronglogin
@@ -66,10 +66,10 @@ if (isset( $_POST['username']) && $_POST['password']){
         <input type="checkbox" id="remember_me" name="_remember_me" checked/>
         <label for="remember_me">Onthoudt mij(werkt nog niet dus niet appen)</label>
         <br>
-        <input type='submit' name='Submit' value='Submit' />
+        <input type='submit' name='Submit' value='Submit'/>
         <?php
         if ($wronglogin != ""){
-            echo   "<input type = 'submit' name = 'ikbenmijnwwvergeten' value = 'Wachtwoord vergeten' />";
+            echo "<input type = 'submit' name = 'ikbenmijnwwvergeten' value = 'Wachtwoord vergeten' />";
         }
         ?>
 
@@ -79,7 +79,7 @@ if (isset( $_POST['username']) && $_POST['password']){
 
 <form id='add' action="View/Gebruiker/Add.php" accept-charset='UTF-8'>
     <div class="container">
-      <input type='submit' name='Add' value='Registreren' />
+        <input type='submit' name='Add' value='Registreren'/>
     </div>
 </form>
 
