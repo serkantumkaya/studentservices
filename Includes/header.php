a0b6b1403e24db683af89c7c1e42fedb2b95e300
Patrick de Ruijter <phwm.deruijter@avans.student.nl>
2020-05-17 19:43:51 +0200

    Menu ietsje aangepast zodat ik verder kan.

--- Includes/header.php
+++ Includes/header.php
@@ -8,20 +8,27 @@ if ($level>=50){
     $uitvoer = <<<EOD
             <!-- [MENU ITEMS] -->
 <!--            ADMIN heeft admin en normale menu's -->
+<nav id='menu'>
+            <ul  id="nav">
+            <li><a href="/StudentServices/index.php">Home</a></li>
+            <li><a href="/StudentServices/View/Beschikbaarheid/View.php">Beschikbaarheid</a></li>  
+            
+            <li><a href="#">Stamdata</a>
                 <ul>
-                <li><a href="/StudentServices/index.php">Home</a></li>
-                <li><a href="/StudentServices/View/Beschikbaarheid/View.php">Beschikbaarheid</a></li>  
-                <li><a href="/StudentServices/View/School/View.php">School</a></li>
-                <li><a href="/StudentServices/View/Categorie/View.php">Categorie</a></li>
-                <li><a href="/StudentServices/View/Opleiding/View.php">Opleiding</a></li>
-                <li><a href="/StudentServices/View/Gebruiker/View.php">Gebruikers</a></li>
-                <li><a href="/StudentServices/View/Feedback/View.php">Feedback</a></li>
-                <li><a href="/StudentServices/View/Profiel/View.php">Mijn profiel</a></li>
-                <li><a href="/StudentServices/View/Categorie/View.php">Projecten</a></li>
-                <li><a href="/StudentServices/View/Opleiding/View.php">FAQ</a></li>
-                <li><a href="/StudentServices/View/Gebruiker/View.php">IETS</a></li>
-                <li><a href="/StudentServices/uitlog.php">Uitloggen</a></li>
-            </ul>
+                    <li class="#lisub"><a href="/StudentServices/View/Categorie/View.php">Categorie</a></li>
+                    <li class="#lisub"><a href="/StudentServices/View/Opleiding/View.php">Opleiding</a></li>
+                    <li class="#lisub"><a href="/StudentServices/View/School/View.php">School</a></li>
+                </ul>
+            </li>
+            <li><a href="/StudentServices/View/Gebruiker/View.php">Gebruikers</a></li>
+            <li><a href="/StudentServices/View/Feedback/View.php">Feedback</a></li>
+            <li><a href="/StudentServices/View/Profiel/View.php">Mijn profiel</a></li>
+            <li><a href="/StudentServices/View/Categorie/View.php">Projecten</a></li>
+            <li><a href="/StudentServices/View/Opleiding/View.php">FAQ</a></li>
+            <li><a href="/StudentServices/View/Gebruiker/View.php">IETS</a></li>
+            <li><a href="/StudentServices/uitlog.php">Uitloggen</a></li>
+        </ul>
+</nav>
 EOD;
 } else{
     $uitvoer = <<<EOD
@@ -47,6 +54,7 @@ EOD;
     <!--The viewport is the user's visible area of a web page.-->
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/menu.css">
     <script type="text/javascript" src="/StudentServices/JS/script.js">
     </script>
 </head>
