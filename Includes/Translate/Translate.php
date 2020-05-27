9c010afb8c494b6b4a758beead4db89b2fda348a
Patrick de Ruijter <phwm.deruijter@avans.student.nl>
2020-05-27 20:30:32 +0200

    Translate

--- Includes/Translate/Translate.php
+++ Includes/Translate/Translate.php
@@ -1,11 +1,14 @@
 <?php
-session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+//session_start();
 //put your translations here
 //USE English keys! Be professional.
 class Translate
 {
     //NL,EN
     static  $Translations = [
+
         "inlogPagRememberMe" => ["Onthoud mij","Remember me"],
         "inlogPagGoodButNoLogin" => ["Je wachtwoord was goed echter werkt het doorverwijzen nog niet!",
             "Your credentials were correct. Only the next page is not responding.!"],
@@ -16,6 +19,22 @@ class Translate
         "inlogPagForgotPassword" => ["Wachtwoord vergeten","Forgot password"],
         "inlogRegister" => ["Registreren","Register"],
         "inlogSubmit" => ["Log in","Log on"],
+
+        "gebruikerEmailInvalid" => ["Het emailadres is ongeldig.","Email is invalid."],
+        "gebruikerEmailIsrequired" => ["Het emailadres is verplicht.","Email is required."],
+        "gebruikerUsernameIsrequired" => ["De gebruikersnaam is verplicht.","Username is required."],
+        "gebruikerUsernameInvalid" => ["De gebruikernaam is ongeldig. Gebruik geen leestekens of spaties."
+            ,"The username is invalid. Do not use punctuation marks or spaces"],
+        "gebruikerUsernameIsInUse" => ["Gebruikersnaam is al reeds gebruikt. Kies een andere gebruikersnaam.",
+            "This username is already taken. Use another usename."],
+        "gebruikerPasswordsNotEqual" => ["De wachtwoorden zijn niet gelijk.",
+            "The passwords are not equal."],
+        "gebruikerRecordNotSaved" => ["Record niet opgeslagen.","Record not saved"],
+        "gebruikerCreateLogin" => ["Aanmaken login gegevens.","Create a login."],
+        "gebruikerUsernameLabel" => ["Gebruikersnaam *","Username *"],
+        "gebruikerEmailLabel" => ["Email *","Email *"],
+        "gebruikerPasswordLabel" => ["Wachtwoord *","Password *"],
+        "gebruikerPasswordLabelCheck" => ["Wachtwoord controle*","Password check*"],
     ];
 
     public static function GetTranslation(string $key) : string
