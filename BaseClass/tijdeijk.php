550fddc4f9eca80e921083d1305361d474f675be
jelleder <j-d-r@live.nl>
2020-05-28 10:10:09 +0200

    kutbestanden van dirk
    bestand van dirk weg, en mijn eige tijdelijke bestand

--- BaseClass/tijdeijk.php
+++
@@ -1,9 +0,0 @@
-foreach ($reactiecontroller->getByProjectID($projectID) as $reactie){
-echo "
-<div id=\"reactie-venster\">
-    <h3>Gegeven door: ". $gebruikersController->getById($reactie->getGebruikerID()) ."</h3>
-    <div id=\"inhoud\">
-        ". $reactie->getReactie() ."
-    </div>
-</div>";
-}
