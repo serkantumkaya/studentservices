cd354caf5a66649b5189417f8a92b3c7f236a603
dirkvliet <34722298+dirkvliet@users.noreply.github.com>
2020-05-30 20:42:05 +0200

    Update detail.php

--- View/Project/Client/detail.php
+++ View/Project/Client/detail.php
@@ -4,12 +4,34 @@ ini_set('display_errors', 1);
 require_once ($_SERVER['DOCUMENT_ROOT']."/StudentServices/Controller/GebruikerController.php");
 require_once ($_SERVER['DOCUMENT_ROOT']."/StudentServices/Controller/BeschikbaarheidController.php");
 require_once ($_SERVER['DOCUMENT_ROOT']."/StudentServices/Controller/ProfielController.php");
-
-
 $gebruikersController = new GebruikerController($_SESSION['GebruikerID']);
 $projectController = new ProjectController();
 $reactiecontroller = new ReactieController();
 $categoriecontroller = new CategorieController();
+$feedbackcontroller = new FeedbackController();
+$schowfeedbackdetail = 0;
+$project = $projectController->getById($_GET['ProjectID']);
+
+if (isset($_POST["feedback"])){
+    $schowfeedbackdetail = 1;
+}
+
+
+if(isset($_POST["Cijfer"]) && isset($_POST["Feedback"])){
+    $feedbackcontroller->add($project->getProjectID(), $_SESSION["GebruikerID"], $_POST["Cijfer"], $_POST["Feedback"]);
+    $schowfeedbackdetail = 0;
+}
+
+
+$feedback = array();
+$feedback = $feedbackcontroller->getGekregenFeedback($project->getGebruikerID());
+$getlastresult = array();
+foreach ($feedback as $result){
+    $getlastresult[] = $result->getFeedbackID();
+}
+rsort($getlastresult);
+
+
 $projectID = $_GET['ProjectID'];
 $_SESSION["ProjectID"] = $projectID;//need it for beschikbaarheid.
 if ($_POST){
@@ -177,10 +199,59 @@ echo "</p>
                     </div>
 
                 </div>
-                <div id="project-feedback">
-                    //TODO: Dirk moet hier zijn werk inbouwen
-                    Feedback
-                </div>
+            <div id="project-feedback">
+                <?php if (!$schowfeedbackdetail){ ?>
+                    <div id="geef_feedback">
+                        <form action="" method="post">
+                            <input id="feedbackknop" type="submit" value="geef feedback">
+                            <input id="feedback" type="hidden" name="feedback" value="feedback">
+                        </form>
+                    </div>
+                    <div id="title_feedback">
+                        <h3><?= $gebruikersController->getById($project->getGebruikerID())->getGebruikersnaam() ?></h3>
+                    </div>
+                    <div id="gemiddelde_cijfer">
+                        <p>
+                            beoordeling <?= $feedbackcontroller->getGemiddeldeGekregenScore($project->getGebruikerID()) ?></p>
+                    </div>
+                    <?php
+                    $counter = 0;
+                    foreach ($getlastresult as $feedback){
+                        if ($counter<4){
+                            $counter++;
+                            ?>
+                            <div id='feedbacbox'>
+                                <div id='feedbackby'><p>gegeven
+                                        door <?= $gebruikersController->getById($feedbackcontroller->getById($feedback)
+                                            ->getGebruikerID())->getGebruikersnaam() ?></p></div>
+                                <div id='feedbackby'><p>cijfer <?= $feedbackcontroller->getById($feedback)
+                                            ->getCijfer() ?></p></div>
+                                <div><p><?= $feedbackcontroller->getById($feedback)->getReview() ?></p></div>
+                            </div>
+                        <?php }
+                    }
+                }else{ ?>
+                    <form action="" method="post">
+                        <label id="feedbacklabel" for="Feedback">
+                            <h3>geef feedback</h3></label>
+                        <select id="Cijfer" name="Cijfer">";
+                            <?php
+                            for ($i = 1; $i<=10; $i++){
+                                // if ($i != $feedbackcontroller->getById($feedback)->getCijfer()){
+                                ?>
+                                <option value= <?=$i?>><?=$i?></option>;
+                                <?php
+                            }
+                            ?>
+                            <textarea id="feedbackfield" maxlength="200" name="Feedback" cols="1" rows="5"
+                                      placeholder="Max 200 characters" required></textarea>
+                            <input type="submit" name="submitFeedback" value="Plaatsen">
+                    </form>
+                <?php }?>
+
+
+
+        </div>
 
         </div>
 
