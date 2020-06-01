f85b0d8358e64c17de1c0055195269e26cf43202
Hellsystem28 <j-d-r@live.nl>
2020-06-01 14:17:35 +0200

    styling projecten
    projectstyling

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div>© Student Services, 2020
         <a href="/StudentServices/index.php">Home</a>
-        <a href="/StudentServices/View/Opleiding/View.php">FAQ</a>
+        <a href="/StudentServices/View/Veelgesteldevragen/View.php">FAQ</a>
         <a href="/StudentServices/ClientSide/Contact.php">Contact</a>
     </div>
 </div>
