d9f58f5c1a09b9e4caf762e9d2d116e499be6ee5
Patrick de Ruijter <phwm.deruijter@avans.student.nl>
2020-06-01 20:15:45 +0200

    Opleiding

--- View/Opleiding/Edit.php
+++ View/Opleiding/Edit.php
@@ -14,7 +14,7 @@ session_start();
     <!--The viewport is the user's visible area of a web page.-->
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
     <link rel="stylesheet" href="/StudentServices/css/style.css">
-
+    <link rel="stylesheet" href="/StudentServices/css/menu.css">
     <script type="text/javascript" src="/StudentServices/JS/script.js">
     </script>
 </head>
